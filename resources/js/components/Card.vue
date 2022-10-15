354bedddeacfe1bb767fce20ba7fa3940f9d0506
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-15 20:36:44 +0200

    Updated Card Vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="bg-white shadow-card rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2">{{ card.title }}</div>
+    <div class="bg-white shadow-card rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2">
+        <div> {{ card.title }}</div>
+        <div class="flex font-bold opacity-0 group-hover:opacity-100 transition-opacity ease-out duration-500">
+            <div class="text-gray-400 pr-2 hover:text-yellow-700">E</div>
+            <div class="text-gray-400 hover:text-red-700">D</div>
+        </div>
+    </div>
 </template>
 
 <script>
