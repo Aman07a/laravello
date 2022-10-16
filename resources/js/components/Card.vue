af3238d375612d2cd169ed2dffa2850f5625457b
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-16 12:15:20 +0200

    Added update() to cardDelete() from Card Vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -13,6 +13,7 @@
 
 <script>
     import CardDelete from "./../graphql/CardDelete.gql";
+    import { EVENT_CARD_DELETED } from './../constants';
 
     export default {
         props: {
@@ -20,11 +21,20 @@
         },
         methods: {
             cardDelete() {
+                const self = this;
+
                 this.$apollo.mutate({
                     mutation: CardDelete,
                     variables: {
                         id: this.card.id
                     },
+                    update(store, { data: { cardDelete } }) {
+                        self.$emit("deleted", { 
+                            store, 
+                            data: cardDelete, 
+                            type: EVENT_CARD_DELETED 
+                        });
+                    }
                 });
             }
         }
