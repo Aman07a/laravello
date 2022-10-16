d1f4b3156db256044672c3acc3f92b67696da3a5
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-16 12:16:24 +0200

    Added  @deleted to <Card> from List Vue

--- resources/js/components/List.vue
+++ resources/js/components/List.vue
@@ -4,7 +4,8 @@
             <div class="text-gray-800 pl-2 pb-2 font-bold">{{ list.title }}</div>
         </div>
 
-        <Card v-for="card in list.cards" :key="card.id" :card="card"></Card>
+        <Card v-for="card in list.cards" :key="card.id" :card="card"
+            @deleted="$emit('card-deleted', {...$event, listId: list.id})"></Card>
 
         <CardEditor v-if="editing" @closed="editing=false" :list="list"
             @added="$emit('card-added', {...$event, listId: list.id})"></CardEditor>
