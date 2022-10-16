296da9f3a6ff0469b44d135b5d87734cbed25de9
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-16 19:39:59 +0200

    Added cardUpdate() to Card Vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,39 +1,56 @@
 <template>
     <div>
-        <div
+        <div v-if="!editing"
             class="group bg-white shadow-card rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2 flex justify-between">
             <div>{{ card.title }}</div>
             <div class="flex font-bold opacity-0 group-hover:opacity-100 transition-opacity ease-out duration-500">
-                <div class="text-gray-400 pr-2 hover:text-yellow-700">E</div>
+                <div class="text-gray-400 pr-2 hover:text-yellow-700" @click="editing=true">E</div>
                 <div class="text-gray-400 hover:text-red-700" @click="cardDelete">D</div>
             </div>
         </div>
+        <CardEditor v-else v-model="title" class="mb-2" label="Save Card" @closed="editing=false" @saved="cardUpdate">
+        </CardEditor>
     </div>
 </template>
 
 <script>
     import CardDelete from "./../graphql/CardDelete.gql";
-    import { EVENT_CARD_DELETED } from './../constants';
+    import CardUpdate from "./../graphql/CardUpdate.gql";
+    import { EVENT_CARD_DELETED, EVENT_CARD_UPDATED } from "./../constants";
+    import CardEditor from "./CardEditor";
 
     export default {
+        components: { CardEditor },
         props: {
             card: Object
+        },
+        data() {
+            return {
+                editing: false,
+                title: this.card.title
+            };
         },
         methods: {
             cardDelete() {
                 const self = this;
 
+            },
+            cardUpdate() {
+                const self = this;
+
                 this.$apollo.mutate({
-                    mutation: CardDelete,
+                    mutation: CardUpdate,
                     variables: {
-                        id: this.card.id
+                        id: this.card.id,
+                        title: this.title
                     },
-                    update(store, { data: { cardDelete } }) {
-                        self.$emit("deleted", {
+                    update(store, { data: cardUpdate }) {
+                        self.$emit("updated", {
                             store,
-                            data: cardDelete,
-                            type: EVENT_CARD_DELETED
+                            data: cardUpdate,
+                            type: EVENT_CARD_UPDATED
                         });
+                        self.editing = false;
                     }
                 });
             }
