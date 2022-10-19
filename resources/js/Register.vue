c71a52a1d01a0f5e64108e69116424a2c7f51156
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-19 19:18:05 +0200

    Added register() to Register Vue

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -10,12 +10,13 @@
                     Signup to your account
                 </div>
 
-                <form>
+                <form @submit.prevent="register">
                     <div class="w-full mb-4">
                         <input
                             type="text"
                             class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
                             placeholder="Enter email"
+                            v-model="email"
                         />
                     </div>
 
@@ -24,6 +25,7 @@
                             type="text"
                             class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
                             placeholder="Enter full name"
+                            v-model="name"
                         />
                     </div>
 
@@ -32,6 +34,7 @@
                             type="password"
                             class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
                             placeholder="Enter password"
+                            v-model="password"
                         />
                     </div>
 
@@ -58,6 +61,40 @@
         </div>
     </div>
 </template>
+
+<script>
+import Register from "./graphql/Register.gql";
+import { gqlErrors } from "./utils";
+
+export default {
+    data() {
+        return {
+            email: null,
+            password: null,
+            name: null,
+            errors: [],
+        };
+    },
+    methods: {
+        async register() {
+            this.errors = [];
+            try {
+                await this.$apollo.mutate({
+                    mutation: Register,
+                    variables: {
+                        email: this.email,
+                        password: this.password,
+                        name: this.name,
+                    },
+                });
+            } catch (err) {
+                this.errors = gqlErrors(err);
+            }
+            this.$router.push({ name: "board" });
+        },
+    },
+};
+</script>
 
 <style scoped>
 .container {
