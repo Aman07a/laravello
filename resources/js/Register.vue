279de2257ef137313445cb483b9eca93a371b701
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-20 17:45:27 +0200

    Updated Register Vue

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -82,7 +82,7 @@ export default {
         async register() {
             this.errors = [];
             try {
-                await this.$apollo.mutate({
+                const response = await this.$apollo.mutate({
                     mutation: Register,
                     variables: {
                         email: this.email,
@@ -90,8 +90,14 @@ export default {
                         name: this.name,
                     },
                 });
-                this.$store.dispatch("setLoggedIn", true);
-                this.$router.push({ name: "board" });
+
+                const user = response.data?.register;
+
+                if (user) {
+                    this.$store.dispatch("setLoggedIn", true);
+                    this.$store.commit("setUser", user);
+                    this.$router.push({ name: "board" });
+                }
             } catch (err) {
                 this.errors = gqlErrors(err);
             }
