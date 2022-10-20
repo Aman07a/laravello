31610de5596d5dfa9fb6a649f65d9079f52b13be
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-20 21:05:38 +0200

    Added booted() to Board.php

--- app/Board.php
+++ app/Board.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\User;
 use App\CardList;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -20,5 +21,14 @@ class Board extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    protected static function booted()
+    {
+        static::creating(function (Board $board) {
+            if (Auth::user()) {
+                $board->owner()->associate(Auth::user());
+            }
+        });
     }
 }
